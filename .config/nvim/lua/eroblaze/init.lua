006775448d2aa7b81fa291a57b58a9e0d4bb166d
Paul Eboselume <eboselumepaul@gmail.com>
2024-05-19 07:49:14 +0100

    Added more stuffs to nvim config

--- .config/nvim/lua/eroblaze/init.lua
+++ .config/nvim/lua/eroblaze/init.lua
@@ -3,37 +3,14 @@ require("eroblaze.keymaps")
 require("eroblaze.lazy_init")
 require("eroblaze.autocmds")
 
--- BORDER
--- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
--- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
---   opts = opts or {}
---   opts.border = opts.border or "rounded"
---   opts.wrap = true
---   -- opts.max_height = opts.max_height or 7
---   -- return orig_util_open_floating_preview(contents, syntax, opts, ...)
---   local bufnr, winid = orig_util_open_floating_preview(contents, syntax, opts, ...)
---   print(bufnr, winid)
--- end
--- BORDER
+-- BORDER FOR HOVER AND SIGNATURE_HELP DOCUMENTATION
 
--- local orig_util_stylize_markdown = vim.lsp.util.stylize_markdown
--- function vim.lsp.util.stylize_markdown(bufnr, contents, opts)
---   print("USING!")
---   opts = opts or {}
---   opts.separator = "||"
---   return orig_util_stylize_markdown(bufnr, contents, opts)
--- end
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 
-local _border = "rounded"
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or "rounded"
+  -- opts.max_height = opts.max_height or 7
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = _border,
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = _border,
-})
-
-vim.diagnostic.config({
-  float = { border = _border },
-})
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
