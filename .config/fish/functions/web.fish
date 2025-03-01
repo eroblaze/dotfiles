eb117d4f2a4a64c5f037f0b036ef1c3d8cd0eb43
Paul Eboselume <eboselumepaul@gmail.com>
2025-03-01 08:10:18 +0100

    update

---
+++ .config/fish/functions/web.fish
@@ -0,0 +1,4 @@
+function web --wraps='cd ~/Desktop/webdev' --wraps='cd Desktop/webdev' --description 'alias web=cd Desktop/webdev'
+  cd Desktop/webdev $argv
+        
+end
--- .config/nvim/lua/eroblaze/lazy/colorscheme.lua
+++ .config/nvim/lua/eroblaze/lazy/colorscheme.lua
@@ -25,31 +25,31 @@ return {
   --   end,
   -- },
 
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        dim_inactive_windows = true,
-        styles = {
-          -- transparency = true,
-        },
-      })
-
-      vim.cmd("colorscheme rose-pine")
-    end,
-  },
-
   -- {
-  --   "craftzdog/solarized-osaka.nvim",
-  --   name = "solarized-osaka",
-  --   lazy = false,
-  --   priority = 1000,
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
   --   config = function()
-  --     require("solarized-osaka").setup({
-  --       transparent = false,
+  --     require("rose-pine").setup({
+  --       dim_inactive_windows = true,
+  --       styles = {
+  --         -- transparency = true,
+  --       },
   --     })
-  --     vim.cmd("colorscheme solarized-osaka")
+  --
+  --     vim.cmd("colorscheme rose-pine")
   --   end,
   -- },
+
+  {
+    "craftzdog/solarized-osaka.nvim",
+    name = "solarized-osaka",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("solarized-osaka").setup({
+        transparent = false,
+      })
+      vim.cmd("colorscheme solarized-osaka")
+    end,
+  },
 }
