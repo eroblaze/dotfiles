409076a0540090d19bdc9524bbdf2b7c2b13cb1e
Paul Eboselume <eboselumepaul@gmail.com>
2025-08-15 19:33:58 +0100

    Added clangd file

---
+++ .config/fish/functions/cf.fish
@@ -0,0 +1,4 @@
+function cf --wraps='cd programming/cf' --wraps='cd programming\\/cf' --wraps='cd ~/programming/cf' --description 'alias cf=cd ~/programming/cf'
+  cd ~/programming/cf $argv
+        
+end
